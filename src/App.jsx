45ba7d326d0c4dc7cf7ae20ccd1e88e8e6ca113b
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-07-21 13:27:11 +0200

    feat: add list rendering with .map and .filter

--- src/App.jsx
+++ src/App.jsx
@@ -8,30 +8,81 @@ import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
+  const cities = [
+    {
+      id: 0,
+      name: "Tokio",
+      description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, iure.",
+      imgUrl:"https://images.unsplash.com/photo-1498036882173-b41c28a8ba34?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      isVisited: true,
+    },
+    {
+      id: 1,
+      name: "New York",
+      description : "Lorem ipsum dolor sit amet.",
+      imgUrl:"https://images.unsplash.com/photo-1516893842880-5d8aada7ac05?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bmV3JTIweW9ya3xlbnwwfHwwfHx8MA%3D%3D",
+      isVisited: false,
+    },
+    {
+      id: 2,
+      name: "Rome",
+      description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis officiis facilis odio asperiores assumenda? Quisquam.",
+      imgUrl:"https://images.unsplash.com/photo-1598258500419-5d7895465a20?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      isVisited: true,
+    },
+    {
+      id: 3,
+      name: "Paris",
+      description : "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+      imgUrl:"https://images.unsplash.com/photo-1522093007474-d86e9bf7ba6f?q=80&w=1600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      isVisited: false,
+    },
+  ]
 
   return (
     <>
     <div className='grid grid-cols-4 gap-10'>
-      <Card
-        isVisited={true}
-        title="Tokio" 
-        imgUrl="https://images.unsplash.com/photo-1498036882173-b41c28a8ba34?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, iure.
+      {cities.map((city) => (
+        <Card
+          key={city.id}
+          title={city.name}
+          isVisited={city.isVisited}
+          imgUrl={city.imgUrl}>
+            {city.description}
+        </Card>
+      ))}
+      {cities
+        .filter((city) => city.isVisited)
+        .map((city) => (
+          <Card
+            key={city.id}
+            title={city.name}
+            isVisited={city.isVisited}
+            imgUrl={city.imgUrl}>
+              {city.description}
+          </Card>
+        ))}
+      {/* <Card
+        isVisited={cities[0].isVisited}
+        title={cities[0].name} 
+        imgUrl={cities[0].imgUrl}>
+          {cities[0].description}
       </Card>
       <Card
         isVisited={false}
         title="New York" 
-        imgUrl="https://images.unsplash.com/photo-1516893842880-5d8aada7ac05?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bmV3JTIweW9ya3xlbnwwfHwwfHx8MA%3D%3D">Lorem ipsum dolor sit amet.
+        imgUrl="https://images.unsplash.com/photo-1516893842880-5d8aada7ac05?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bmV3JTIweW9ya3xlbnwwfHwwfHx8MA%3D%3D">
       </Card>
       <Card
         isVisited={true}
         title="Rome" 
-        imgUrl="https://images.unsplash.com/photo-1598258500419-5d7895465a20?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis officiis facilis odio asperiores assumenda? Quisquam.
+        imgUrl="https://images.unsplash.com/photo-1598258500419-5d7895465a20?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
       </Card>
       <Card
         isVisited={false}
         title="Paris" 
-        imgUrl="https://images.unsplash.com/photo-1522093007474-d86e9bf7ba6f?q=80&w=1600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-      </Card>
+        imgUrl="https://images.unsplash.com/photo-1522093007474-d86e9bf7ba6f?q=80&w=1600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
+      </Card> */}
     </div>
     <Navbar></Navbar>
       <div>
