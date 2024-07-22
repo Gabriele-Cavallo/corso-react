1b7a9a2ab06fbb59626a77ab4b0783487c5db1e7
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-07-22 11:13:32 +0200

    feat: add event function

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,19 @@ import './components/Navbar'
 import Navbar from './components/Navbar'
 import Card from './components/Card'
 
+function handleClick() {
+  alert('ciao');
+}
+
+function handleChange(event) {
+  console.log(event.target.value);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  console.log(event);
+}
+
 function App() {
   const [count, setCount] = useState(0)
   const cities = [
@@ -85,7 +98,7 @@ function App() {
       </Card> */}
     </div>
     <Navbar></Navbar>
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -93,18 +106,25 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React</h1> */}
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <button onClick={handleClick}>
+          alert
+        </button>
+        <input onChange={handleChange} type="text" />
+        <form onSubmit={handleSubmit} action="">
+          <button type="submit">Submit</button>
+        </form>
+        {/* <p>
           Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        </p> */}
       </div>
-      <p className="read-the-docs">
+      {/* <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
